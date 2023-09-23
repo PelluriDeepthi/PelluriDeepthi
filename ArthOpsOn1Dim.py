229cc68f30cd416fb9b8c9f540aaebcce5b3642e
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-09-23 10:41:23 +0530

    Update ArthOpsOn1Dim.py

--- ArthOpsOn1Dim.py
+++ ArthOpsOn1Dim.py
@@ -21,3 +21,5 @@ print("\nThe Remainder of",zeroDarray1, "and", zeroDarray2 ,"is:", resultarray)
 
 resultarray = zeroDarray1 // zeroDarray2
 print("\nThe FloorDivision of",zeroDarray1, "and", zeroDarray2 ,"is:", resultarray)
+
+print("The square of",zeroDarray1, "is:", zeroDarray1**3)
