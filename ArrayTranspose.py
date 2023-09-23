4d9b220c6c2220919f468b729befd3e4698a84af
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-09-23 12:26:38 +0530

    Add files via upload

---
+++ ArrayTranspose.py
@@ -0,0 +1,8 @@
+import numpy as np
+twoDArray = np.array([[1, 2, 3], [4, 5, 6]])
+print("The transpose Array is:", twoDArray.transpose)
+print("Shape of Transpose Array is:", (twoDArray.transpose()).shape)
+print("The reshape of an Array is:", reshape(6, 1))
+
+sortedArray = twoDArray.sort()
+print("The sorted array is:", sortedArray)
