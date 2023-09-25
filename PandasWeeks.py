1283c6fe89b3875cb6ee383e4dc364a2d2e63855
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-09-25 16:08:23 +0530

    Add files via upload

---
+++ PandasWeeks.py
@@ -0,0 +1,7 @@
+import pandas as pd
+weekDict = {1:"Monday", 2:"Tuesday", 3:"Wednesday", 4:"Thursday", 5:"Friday", 6:"Saturday", 7:"Sunday"}
+dictPandas = pd.Series(weekDict)
+print("The series created from Dictionary is:\n", dictPandas)
+
+dictPandas.index = ["day1", "day2", "day3", "day4", "day5", "day6", "day7"]
+print("The selected days is:\n", dictPandas[["day1", "day6"]])
