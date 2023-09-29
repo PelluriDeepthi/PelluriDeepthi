44c5b60701aae3291cb1ecf7f20f80d54babd921
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-09-29 17:23:25 +0530

    Update and rename ArthOps.py to Operators.py

---
+++ Operators.py
@@ -0,0 +1,42 @@
+x = 12
+y = 4
+
+//Arthmetic Operators
+add = x + y
+print("Addition of", x, "and", y, "is:", add)
+sub = x - y
+print("\nSubtraction of", x, "and", y, "is:", sub)
+mul = x * y
+print("\nMultiplication of", x, "and", y, "is:", mul)
+div = x/y
+print("\nDivision of", x, "and", y, "is:", div)
+floor_div = x//y
+print("\nFloor Division of", x, "and", y, "is:", floor_div)
+mod = x % y
+print("\nModulus of", x, "and", y, "is:", mod)
+exp = x ** 3
+print("\nExponential of", x, "is:", exp)
+
+
+//Conditional Operators
+equal = x == y
+print(equal)
+not_equal = x != y
+print(not_equal)
+greater_than = x > y
+print(greater_than)
+lesser_than = x < y
+print(lesser_than)
+
+
+# Logical operators
+X = True
+Y = False
+logical_and = X and Y
+print(logical_and)
+logical_or = X or Y
+print(logical_or)
+logical_notX = not X
+print(logical_notX)
+logical_notY = not Y
+print(logical_notY)
