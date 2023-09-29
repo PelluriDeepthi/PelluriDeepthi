7a22922f7864132b6fed02372a63266ec2c51568
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-09-29 10:37:24 +0530

    Add files via upload

---
+++ ArthOps.py
@@ -0,0 +1,23 @@
+x = 12
+y = 4
+
+add = x + y
+print("Addition of", x, "and", y, "is:", add)
+
+sub = x - y
+print("\nSubtraction of", x, "and", y, "is:", sub)
+
+mul = x * y
+print("\nMultiplication of", x, "and", y, "is:", mul)
+
+div = x/y
+print("\nDivision of", x, "and", y, "is:", div)
+
+floor_div = x//y
+print("\nFloor Division of", x, "and", y, "is:", floor_div)
+
+mod = x % y
+print("\nModulus of", x, "and", y, "is:", mod)
+
+exp = x ** 3
+print("\nExponential of", x, "is:", exp)
