41094b66d1cc71f1171e0f99ac1fdc2379b82a2c
PelluriDeepthi <120254626+PelluriDeepthi@users.noreply.github.com>
2023-10-25 11:58:49 +0530

    Add files via upload

---
+++ RemoveEmptyTupleUsingLoops.py
@@ -0,0 +1,7 @@
+def Remove(tuples):
+    while () in tuples:
+        tuples.remove(())
+        return tuples
+
+tuples = [(), ('Ram', '15', '8'), (), ('Srinivas', 'Lakshmi'), ('Amar', 'Akbar', '45'), ('', ''), ()]
+print(Remove(tuples))
